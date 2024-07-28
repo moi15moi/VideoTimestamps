15e4e1a3f39f34a84984af6b5120f6cceae5cc09
moi15moi <moi15moismokerlolilol@gmail.com>
2024-07-28 12:49:12 -0400

    Correct example

--- examples/get_timestamps.py
+++ examples/get_timestamps.py
@@ -4,12 +4,12 @@ from video_timestamps import TimestampsFactory, TimeType, RoundingMethod
 
 def main() -> None:
     fps = Fraction(24000, 1001)
-    timestamps = TimestampsFactory.from_fps(fps)
-
     # Read the documentation to find out which RoundingMethod suits your needs.
+    timestamps = TimestampsFactory.from_fps(fps, RoundingMethod.FLOOR)
+
     frame = 10
-    start_time = timestamps.frames_to_ms(frame, TimeType.START, RoundingMethod.FLOOR)
-    end_time = timestamps.frames_to_ms(frame, TimeType.END, RoundingMethod.FLOOR)
+    start_time = timestamps.frames_to_ms(frame, TimeType.START)
+    end_time = timestamps.frames_to_ms(frame, TimeType.END)
 
     print(f"For the fps {fps}, the frame {frame} start at {start_time} ms and end at {end_time} ms.")
 
