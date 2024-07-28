1f8fb50fa4ee6702a19720ed5c39c773cbb155dc
moi15moi <moi15moismokerlolilol@gmail.com>
2024-07-28 19:22:34 -0400

    Update example

--- examples/get_timestamps.py
+++ examples/get_timestamps.py
@@ -5,6 +5,7 @@ from video_timestamps import TimestampsFactory, TimeType, RoundingMethod
 def main() -> None:
     fps = Fraction(24000, 1001)
     # Read the documentation to find out which RoundingMethod suits your needs.
+    # You can also create a Timestamps instance with TimestampsFactory.from_timestamps_file and TimestampsFactory.from_video_file
     timestamps = TimestampsFactory.from_fps(fps, RoundingMethod.FLOOR)
 
     frame = 10
