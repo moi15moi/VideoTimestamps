aff430895ed2ece0fa6b7926ad33894aa2b86496
moi15moi <moi15moismokerlolilol@gmail.com>
2024-12-15 20:03:03 -0500

    Refactor Timestamps/TimestampsFactory
    Now, we have FPSTimestamps, VideoTimestamps and TextFileTimestamps.
    They now support different unit of time (milliseconds, nanoseconds, etc)

--- video_timestamps/__init__.py
+++ video_timestamps/__init__.py
@@ -2,7 +2,9 @@
 
 # Files
 from ._version import __version__
+from .abc_timestamps import *
+from .fps_timestamps import *
 from .rounding_method import *
+from .text_file_timestamps import *
 from .time_type import *
-from .timestamps import *
-from .timestamps_factory import *
+from .video_timestamps import *
