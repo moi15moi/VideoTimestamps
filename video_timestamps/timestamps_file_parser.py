b7c02748a76cfd6f7f37e4047f52ba6b71bda974
moi15moi <moi15moismokerlolilol@gmail.com>
2024-12-15 20:01:34 -0500

    Refactor timestamps_file_parser
    Don't round the timestamps.
    The decision or rounding will be taken from ABCTimestamps.

--- video_timestamps/timestamps_file_parser.py
+++ video_timestamps/timestamps_file_parser.py
@@ -1,7 +1,7 @@
-from .rounding_method import RoundingMethod
 from fractions import Fraction
 from io import TextIOWrapper
 from re import compile
+from typing import Optional
 
 
 class RangeV1:
@@ -13,76 +13,56 @@ class RangeV1:
 
 class TimestampsFileParser:
     @staticmethod
-    def parse_file(
-        file_content: TextIOWrapper, rounding_method: RoundingMethod
-    ) -> tuple[list[int], Fraction, Fraction]:
+    def parse_file(file_content: TextIOWrapper) -> tuple[list[Fraction], Optional[Fraction]]:
         """Parse timestamps from a [timestamps file](https://mkvtoolnix.download/doc/mkvmerge.html#mkvmerge.external_timestamp_files) and return them.
 
         Inspired by: https://gitlab.com/mbunkus/mkvtoolnix/-/blob/72dfe260effcbd0e7d7cf6998c12bb35308c004f/src/merge/timestamp_factory.cpp#L27-74
 
         Parameters:
-            file_content (TextIOWrapper): The timestamps content
-            rounding_method (RoundingMethod): A rounding method
+            file_content (TextIOWrapper): The timestamps content.
 
         Returns:
             A tuple containing these 3 informations:
-                1. A list of each timestamps rounded or floored to milliseconds.
-                2. The last timestamps not rounded
-                3. If the format of timestamps is 1, then it return the fpms of the Assume section
-                   If the format of timestamps is 2 or 4, then it approximate the fpms.
-                        It calculate the fps like this: (1000 * nbr_frame) / (last_timestamps - first_timestamps)
+                1. A list of each frame timestamps (in milliseconds).
+                2. The fps (if supported by the timestamps file format).
         """
 
-        regex_timestamps = compile("^# time(?:code|stamp) *format v(\\d+).*")
-
+        regex_timestamps = compile("^# *time(?:code|stamp) *format v(\\d+).*")
         line = file_content.readline()
         match = regex_timestamps.search(line)
         if match is None:
-            raise ValueError(
-                f'The timestamps at line 0 is invalid. Here is the line: "{line}"'
-            )
+            raise ValueError("The line 0 is invalid doesn't contain the version of the timestamps file.")
 
         version = int(match.group(1))
 
         if version == 1:
-            timestamps, last_frame_time, fpms = TimestampsFileParser._parse_v1_file(
-                file_content, rounding_method
-            )
+            timestamps, fps = TimestampsFileParser._parse_v1_file(file_content)
         elif version == 2 or version == 4:
-            (
-                timestamps,
-                last_frame_time,
-                fpms,
-            ) = TimestampsFileParser._parse_v2_and_v4_file(
-                file_content, version, rounding_method
-            )
+            timestamps = TimestampsFileParser._parse_v2_and_v4_file(file_content, version)
+            fps = None
         else:
             raise NotImplementedError(
-                f"The file uses version {version} for its timestamps, but this format is currently not compatible with PyonFX."
+                f"The file uses version {version}, but this format is currently not supported."
             )
 
-        return timestamps, last_frame_time, fpms
+        return timestamps, fps
 
 
     @staticmethod
-    def _parse_v1_file(
-        file_content: TextIOWrapper, rounding_method: RoundingMethod
-    ) -> tuple[list[int], Fraction, Fraction]:
+    def _parse_v1_file(file_content: TextIOWrapper) -> tuple[list[Fraction], Fraction]:
         """Create timestamps based on the timestamps v1 file provided.
 
         Inspired by: https://gitlab.com/mbunkus/mkvtoolnix/-/blob/72dfe260effcbd0e7d7cf6998c12bb35308c004f/src/merge/timestamp_factory.cpp#L82-175
 
         Parameters:
             file_content (TextIOWrapper): The timestamps content
-            rounding_method (RoundingMethod): A rounding method
 
         Returns:
-            A tuple containing these 3 informations:
-                1. A list of each timestamps rounded to milliseconds
-                2. The last timestamps not rounded
-                3. The fpms (frame(s) per milliseconds)
+            A tuple containing these 2 informations:
+                1. A list of each frame timestamps (in milliseconds).
+                2. The fps.
         """
-        timestamps: list[int] = []
+        timestamps: list[Fraction] = []
         ranges_v1: list[RangeV1] = []
         line: str = ""
 
@@ -154,24 +134,23 @@ class TimestampsFileParser:
                 raise ValueError("Override ranges must not overlap.")
 
             while frame < range_v1.start_frame:
-                timestamps.append(rounding_method(time))
+                timestamps.append(time)
                 time += Fraction(1000) / default_fps
                 frame += 1
 
             while frame <= range_v1.end_frame:
-                timestamps.append(rounding_method(time))
+                timestamps.append(time)
                 time += Fraction(1000) / range_v1.fps
                 frame += 1
 
-        timestamps.append(rounding_method(time))
-        fpms = default_fps / Fraction(1000)
-        return timestamps, time, fpms
+        timestamps.append(time)
+        return timestamps, default_fps
 
 
     @staticmethod
     def _parse_v2_and_v4_file(
-        file_content: TextIOWrapper, version: int, rounding_method: RoundingMethod
-    ) -> tuple[list[int], Fraction, Fraction]:
+        file_content: TextIOWrapper, version: int
+    ) -> list[Fraction]:
         """Create timestamps based on the timestamps v2 or v4 file provided.
 
         Inspired by: https://gitlab.com/mbunkus/mkvtoolnix/-/blob/72dfe260effcbd0e7d7cf6998c12bb35308c004f/src/merge/timestamp_factory.cpp#L201-267
@@ -179,22 +158,16 @@ class TimestampsFileParser:
         Parameters:
             file_content (TextIOWrapper): The timestamps content
             version (int): The version of the timestamps (only 2 or 4 is allowed)
-            rounding_method (RoundingMethod): A rounding method
 
         Returns:
-            A tuple containing these 3 informations:
-                1. A list of each timestamps rounded to milliseconds
-                2. The last timestamps not rounded
-                3. The fpms (frame(s) per milliseconds)
+          A list of each frame timestamps (in milliseconds).
         """
 
         if version not in (2, 4):
             raise ValueError("You can only specify version 2 or 4.")
 
-        timestamps: list[int] = []
-        previous_timestamp: int = 0
-        lowest_timestamp: Fraction = None # type: ignore
-        highest_timestamp: Fraction = None # type: ignore
+        timestamps: list[Fraction] = []
+        previous_timestamp: Optional[Fraction] = None
 
         for line in file_content:
             line = line.strip(" \t")
@@ -209,20 +182,13 @@ class TimestampsFileParser:
                     f'The timestamps file contain a invalid line. Here is it: "{line}"'
                 )
 
-            if highest_timestamp is None or highest_timestamp < timestamp:
-                highest_timestamp = timestamp
-            if lowest_timestamp is None or lowest_timestamp > timestamp:
-                lowest_timestamp = timestamp
-
-            rounded_timestamp = rounding_method(timestamp)
-
-            if version == 2 and rounded_timestamp < previous_timestamp:
+            if version == 2 and previous_timestamp is not None and timestamp < previous_timestamp:
                 raise ValueError(
                     f"The timestamps file contain timestamps NOT in ascending order."
                 )
 
-            previous_timestamp = rounded_timestamp
-            timestamps.append(rounded_timestamp)
+            previous_timestamp = timestamp
+            timestamps.append(timestamp)
 
         if not len(timestamps):
             raise ValueError(f"The timestamps file is empty.")
@@ -230,9 +196,4 @@ class TimestampsFileParser:
         if version == 4:
             timestamps.sort()
 
-        duration = highest_timestamp - lowest_timestamp
-        if duration:
-            fpms = Fraction(len(timestamps) - 1) / (highest_timestamp - lowest_timestamp)
-        else:
-            fpms = Fraction(0)
-        return timestamps, highest_timestamp, fpms
+        return timestamps
