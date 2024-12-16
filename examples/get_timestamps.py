4540d4b98645952c815ef1c409e884dac6517502
moi15moi <moi15moismokerlolilol@gmail.com>
2024-12-15 20:13:35 -0500

    Update example with the new module

--- examples/get_timestamps.py
+++ examples/get_timestamps.py
@@ -1,18 +1,24 @@
 from fractions import Fraction
-from video_timestamps import TimestampsFactory, TimeType, RoundingMethod
+from video_timestamps import FPSTimestamps, TimeType, RoundingMethod
 
 
 def main() -> None:
     fps = Fraction(24000, 1001)
+    time_scale = Fraction(1000)
     # Read the documentation to find out which RoundingMethod suits your needs.
-    # You can also create a Timestamps instance with TimestampsFactory.from_timestamps_file and TimestampsFactory.from_video_file
-    timestamps = TimestampsFactory.from_fps(fps, RoundingMethod.FLOOR)
+    # You can also create a Timestamps instance with TextFileTimestamps, VideoTimestamps
+    timestamps = FPSTimestamps(RoundingMethod.ROUND, time_scale, fps)
 
     frame = 10
-    start_time = timestamps.frames_to_ms(frame, TimeType.START)
-    end_time = timestamps.frames_to_ms(frame, TimeType.END)
+    # Max precision
+    start_time_in_seconds = timestamps.frame_to_time(frame, TimeType.START)
+    end_time_in_seconds = timestamps.frame_to_time(frame, TimeType.END)
+    print(f"For the fps {fps}, the frame {frame} start at {start_time_in_seconds} s and end at {end_time_in_seconds} s.")
 
-    print(f"For the fps {fps}, the frame {frame} start at {start_time} ms and end at {end_time} ms.")
+    start_time_in_ms = timestamps.frame_to_time(frame, TimeType.START, 3)
+    end_time_in_ms = timestamps.frame_to_time(frame, TimeType.END, 3)
+    print(f"For the fps {fps}, the frame {frame} start at {start_time_in_ms} ms and end at {end_time_in_ms} ms.")
+
 
 if __name__ == "__main__":
     main()
