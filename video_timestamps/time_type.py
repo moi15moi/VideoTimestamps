773f30685aa6cab074d1113392340acfd7a9a880
moi15moi <moi15moismokerlolilol@gmail.com>
2025-01-02 17:48:17 -0500

    [time_type] Use the same interval as in the documentation proof

--- video_timestamps/time_type.py
+++ video_timestamps/time_type.py
@@ -6,7 +6,7 @@ class TimeType(Enum):
     """
     Represents different types of time intervals for video frames in a player.
 
-    When working with a video that has a frame rate of 24000/1001 fps and using the RoundingMethod.ROUND,
+    When working with a video that has a frame rate of 24000/1001 fps, using the RoundingMethod.ROUND and with the Timescale 1000,
     the first 4 frames will start at the following times in a video player:
         - Frame 0: 0 ms
         - Frame 1: 42 ms
@@ -17,42 +17,45 @@ class TimeType(Enum):
 
     1. EXACT:
        Corresponds to the precise frame time in the video player.
-       Each frame has an interval: [Current_frame_time_ms, Next_frame_time_ms - 1]
+       Each frame has an interval: [Current_frame_time, Next_frame_time[
 
        Example:
            - fps = 24000/1001
            - rounding_method = RoundingMethod.ROUND
+           - time_scale = 1000
 
            Frame intervals:
-               - Frame 0: [0, 41] ms
-               - Frame 1: [42, 82] ms
-               - Frame 2: [83, 124] ms
+               - Frame 0: [0, 42[ ms
+               - Frame 1: [42, 83[ ms
+               - Frame 2: [83, 125[ ms
 
     2. START:
        Corresponds to the start time of the subtitle.
-       Each frame has an interval: [Previous_frame_time_ms + 1, Current_frame_time_ms]
+       Each frame has an interval: ]Previous_frame_time, Current_frame_time]
 
        Example:
            - fps = 24000/1001
            - rounding_method = RoundingMethod.ROUND
+           - time_scale = 1000
 
            Frame intervals:
                - Frame 0: 0 ms
-               - Frame 1: [1, 42] ms
-               - Frame 2: [43, 83] ms
+               - Frame 1: ]0, 42] ms
+               - Frame 2: ]42, 83] ms
 
     3. END:
        Corresponds to the end time of the subtitle.
-       Each frame has an interval: [Current_frame_time_ms + 1, Next_frame_time_ms]
+       Each frame has an interval: ]Current_frame_time, Next_frame_time]
 
        Example:
            - fps = 24000/1001
            - rounding_method = RoundingMethod.ROUND
+           - time_scale = 1000
 
            Frame intervals:
-               - Frame 0: [1, 42] ms
-               - Frame 1: [43, 83] ms
-               - Frame 2: [84, 125] ms
+               - Frame 0: ]0, 42] ms
+               - Frame 1: ]42, 83] ms
+               - Frame 2: ]83, 125] ms
     """
 
     START = "START"
