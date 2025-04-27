6812524b529974026693caaeb0e8b94bb2344c4b
moi15moi <moi15moismokerlolilol@gmail.com>
2025-04-27 17:20:03 -0400

    Add TimeUnitConverter
    It is useful for people who don't have a time scale but want to instantiate an FPSTimestamps object using a time base or a timestamp scale.

--- video_timestamps/__init__.py
+++ video_timestamps/__init__.py
@@ -7,4 +7,5 @@ from .fps_timestamps import *
 from .rounding_method import *
 from .text_file_timestamps import *
 from .time_type import *
+from .time_unit_converter import *
 from .video_timestamps import *
