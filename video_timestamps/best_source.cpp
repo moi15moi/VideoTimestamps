6e5b64735a8f883fa9b4240a48d14eb64591a915
moi15moi <80980684+moi15moi@users.noreply.github.com>
2025-06-24 19:27:46 -0400

    Have ffms2 fonctionnal

--- video_timestamps/best_source.cpp
+++ video_timestamps/best_source.cpp
@@ -82,9 +82,7 @@ pybind11::tuple get_pts(const std::string &filename, int index) {
 }
 
 
-pybind11::tuple ffms2_get_pts(const std::string &filename, int TrackNumber) {
-    FFMS_Init(0, 0);
-
+pybind11::tuple ffms2_get_pts(const std::string &filename, int index) {
     char errmsg[1024];
     FFMS_ErrorInfo errinfo;
     errinfo.Buffer      = errmsg;
@@ -92,70 +90,84 @@ pybind11::tuple ffms2_get_pts(const std::string &filename, int TrackNumber) {
     errinfo.ErrorType   = FFMS_ERROR_SUCCESS;
     errinfo.SubType     = FFMS_ERROR_SUCCESS;
     
-    FFMS_Indexer* indexer = FFMS_CreateIndexer(filename.c_str(), &errinfo);
-    if (!indexer) {
-        /* handle error (print errinfo.Buffer somewhere) */
+    FFMS_Init(0, 0);
+
+    FFMS_Indexer *indexer = FFMS_CreateIndexer(filename.c_str(), &errinfo);
+    if (!indexer)
+        throw std::runtime_error(std::format("ffms2 reported an error while calling FFMS_CreateIndexer: {}.", std::string(errinfo.Buffer)));    
+
+    int num_tracks = FFMS_GetNumTracksI(indexer);
+    if (index >= num_tracks)
+        throw std::invalid_argument(std::format("The index {} is not in the file {}.", index, filename));    
+
+    int track_type = FFMS_GetTrackTypeI(indexer, index);
+    if (track_type != FFMS_TYPE_VIDEO) {
+        std::string steam_media_type = "";
+        switch (track_type) {
+            case FFMS_TYPE_AUDIO:
+                steam_media_type = "audio";
+                break;
+            case FFMS_TYPE_DATA:
+                steam_media_type = "data";
+                break;
+            case FFMS_TYPE_SUBTITLE:
+                steam_media_type = "subtitle";
+                break;
+            case FFMS_TYPE_ATTACHMENT:
+                steam_media_type = "attachment";
+                break;
+            default:
+                steam_media_type = "unknown";
+                break;
+        }
+
+        throw std::invalid_argument(std::format("The index {} is not a video stream. It is an \"{}\" stream.", index, steam_media_type));    
     }
 
-    // FFMS_GetNumTracks
-    // FFMS_GetTrackType
-
-    auto index = std::unique_ptr<FFMS_Index, void(*)(FFMS_Index*)>(
+    auto ffms2_index = std::unique_ptr<FFMS_Index, void(*)(FFMS_Index*)>(
         FFMS_DoIndexing2(indexer, FFMS_IEH_ABORT, &errinfo),
         FFMS_DestroyIndex
     );
+    if (!ffms2_index)
+        throw std::runtime_error(std::format("ffms2 reported an error while calling FFMS_DoIndexing2: {}.", errinfo.Buffer));    
 
-    if (!index) {
-        /* handle error (you should know what to do by now) */
-    } 
-    int Threads = 1;
-    int SeekMode = FFMS_SEEK_NORMAL;
-    FFMS_ErrorInfo ErrInfo; 
+    int threads = 1;
+    int seek_mode = FFMS_SEEK_NORMAL;
     auto video_source = std::unique_ptr<FFMS_VideoSource, void(*)(FFMS_VideoSource*)>(
-        FFMS_CreateVideoSource(filename.c_str(), TrackNumber, index.get(), Threads, SeekMode, &ErrInfo),
+        FFMS_CreateVideoSource(filename.c_str(), index, ffms2_index.get(), threads, seek_mode, &errinfo),
         FFMS_DestroyVideoSource
     );
-
 	if (!video_source)
-        throw std::invalid_argument(std::format("Failed to open video track: {}", ErrInfo.Buffer));    
+        throw std::runtime_error(std::format("ffms2 reported an error while calling FFMS_CreateVideoSource: {}", errinfo.Buffer));    
 
 	FFMS_Track *track = FFMS_GetTrackFromVideo(video_source.get());
-	if (track == nullptr)
-        throw std::invalid_argument("failed to get frame data");    
-
-
+	if (!track)
+        throw std::runtime_error("ffms2 reported an error while calling FFMS_GetTrackFromVideo");    
 
 
     const FFMS_VideoProperties *videoprops = FFMS_GetVideoProperties(video_source.get());
 
-    
-
     std::vector<int64_t> pts_list;
-	for (int CurFrameNum = 0; CurFrameNum < videoprops->NumFrames; CurFrameNum++) {
-		const FFMS_FrameInfo *CurFrameData = FFMS_GetFrameInfo(track, CurFrameNum);
-		if (!CurFrameData)
-			throw std::invalid_argument(std::format("Couldn't get info about frame {}", CurFrameNum));
+    for (int n = 0; n < videoprops->NumFrames; n++) {
+		const FFMS_FrameInfo *frame_info = FFMS_GetFrameInfo(track, n);
+		if (!frame_info)
+			throw std::runtime_error(std::format("ffms2 reported an error while calling FFMS_GetFrameInfo with frame {}", n));
 
-        pts_list.push_back(CurFrameData->PTS);
+        pts_list.push_back(frame_info->PTS);
 	}
 
-
-
-
-
-	const FFMS_TrackTimeBase *TimeBase = FFMS_GetTimeBase(track);
-	if (TimeBase == nullptr)
-        throw std::invalid_argument("failed to get track time base");    
-
+	const FFMS_TrackTimeBase *ffms2_time_base = FFMS_GetTimeBase(track);
+	if (!ffms2_time_base)
+        throw std::runtime_error("ffms2 reported an error while calling FFMS_GetTimeBase");
 
     pybind11::object fraction_class = pybind11::module_::import("fractions").attr("Fraction");
-    pybind11::object time_base = fraction_class(TimeBase->Num, TimeBase->Den);
+    pybind11::object time_base = fraction_class(ffms2_time_base->Num, ffms2_time_base->Den) / fraction_class(1000, 1);
     pybind11::object fps = fraction_class(videoprops->FPSNumerator, videoprops->FPSDenominator);
 
     return pybind11::make_tuple(pts_list, time_base, fps);
 }
 
 PYBIND11_MODULE(best_source, m) {
-    m.def("get_pts", &get_pts, pybind11::arg("filename"), pybind11::arg("index"));
-    m.def("ffms2_get_pts", &ffms2_get_pts, pybind11::arg("filename"), pybind11::arg("index"));
+    m.def("bestsource_get_pts", &get_pts, pybind11::arg("filename"), pybind11::arg("index"));
+    m.def("get_pts", &ffms2_get_pts, pybind11::arg("filename"), pybind11::arg("index"));
 }
